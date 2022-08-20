550720b7eaacee3b8c735831f06f5a95e820024c
israela <israela.zimru@tipranks.com>
2022-08-20 18:55:12 +0300

    first commit

---
+++ client/src/App.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+
+function App() {
+  return (
+    <Container className="text-center mt-4">
+      <h1 className="display-3">My Shooping List 😊</h1>
+    </Container>
+  );
+}
+
+export default App;
